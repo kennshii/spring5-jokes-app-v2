60c47f921603b19180ff8315391596c4f9f015e6
eduard <edwardbs876@gmail.com>
2022-11-03 19:29:40 +0200

    thymeleaf template for view index

--- src/main/java/gg/kennshi/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/gg/kennshi/spring5jokesappv2/controllers/JokeController.java
@@ -1,16 +1,15 @@
 package gg.kennshi.spring5jokesappv2.controllers;
 
 import gg.kennshi.spring5jokesappv2.services.JokeService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JokesController {
+public class JokeController {
     private final JokeService jokeService;
 
-    public JokesController(@Qualifier("jokeServiceImpl") JokeService jokeService) {
+    public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
